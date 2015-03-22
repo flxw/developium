ef4e3417e7c590668d5a7978e4111821b2bd55b4
Felix Wolff <felix.stefan.wolff@gmail.com>
2015-03-22 16:54:45 +0100

    Finish desktop size

--- Gruntfile.js
+++ Gruntfile.js
@@ -7,28 +7,6 @@ module.exports = function(grunt) {
   grunt.loadNpmTasks('grunt-contrib-stylus')
 
   grunt.initConfig({
-    /*connect: {
-      'all': {
-        options:{
-          port: 9999,
-          hostname: "0.0.0.0",
-          // No need for keepalive anymore as watch will keep Grunt running
-          //keepalive: true,
-
-          // Livereload needs connect to insert a cJavascript snippet
-          // in the pages it serves. This requires using a custom connect middleware
-          middleware: function(connect, options) {
-            return [
-              // Load the middleware provided by the livereload plugin
-              // that will take care of inserting the snippet
-              require('grunt-contrib-livereload/lib/utils').livereloadSnippet,
-              // Serve the project folder
-              connect.static(options.base)
-            ];
-          }
-        }
-      }
-    }*/
     'http-server': {
       dev: {
         root: './',
@@ -45,7 +23,7 @@ module.exports = function(grunt) {
       all:['js/*.js']
     },
     'watch': {
-      files:['index.html', 'css/*.styl'],
+      files:['*.html', 'css/*.styl'],
       tasks:['stylus'],
       options: {
         spawn: false,
@@ -58,7 +36,7 @@ module.exports = function(grunt) {
           urlfunc: 'embedurl' // use embedurl('test.png') in our code to trigger Data URI embedding
         },
         files: {
-          'css/style.css': ['css/sidebar.styl', 'css/general.styl']
+          'css/style.css': ['css/*.styl', 'css/normalize.css']
         }
       }
     }
