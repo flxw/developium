fc7098989b0f6a3710270e8ab952b71dfa1454b2
Felix Wolff <felix.stefan.wolff@gmail.com>
2015-03-22 21:26:11 +0100

    Prepare for 0.1 release

--- Gruntfile.js
+++ Gruntfile.js
@@ -5,6 +5,8 @@ module.exports = function(grunt) {
   grunt.loadNpmTasks('grunt-http-server')
   grunt.loadNpmTasks('grunt-contrib-watch')
   grunt.loadNpmTasks('grunt-contrib-stylus')
+  grunt.loadNpmTasks('grunt-contrib-copy')
+  grunt.loadNpmTasks('grunt-contrib-compress')
 
   grunt.initConfig({
     'http-server': {
@@ -39,9 +41,27 @@ module.exports = function(grunt) {
           'assets/css/style.css': ['assets/css/*.styl', 'assets/css/normalize.css']
         }
       }
+    },
+    'copy': {
+      main: {
+        files: [
+          {expand: true, src: ['assets/**/style.css'], dest: 'build/'},
+          {expand: true, src: ['assets/images/*'], dest: 'build/'},
+          {expand: true, src: ['*.hbs'], dest: 'build/'},
+          {expand: true, src: ['package.json'], dest: 'build/'}
+          ]
+      }
+    },
+    'compress': {
+      main: {
+        options: {
+          archive: 'developium.zip'
+        },
+        src: ['build/**/*']
+      }
     }
   })
 
   grunt.registerTask('default', ['http-server:dev', 'watch'])
-  grunt.registerTask('build', ['stylus'])
+  grunt.registerTask('build', ['stylus', 'copy', 'compress'])
 }
